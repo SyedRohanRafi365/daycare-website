d91cc116c5c08c3e3698a5e25d4906a4f0f62505
SyedRohanRafi365 <156239733+SyedRohanRafi365@users.noreply.github.com>
2026-01-08 21:14:44 -0500

    01082026 fixed site

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -40,21 +40,13 @@ const Contact = () => {
       <div className="contact-col">
         <h3>Send us a message <img src={msg_icon} alt="" /></h3>
         <ul>
-            <li><img src={mail_icon} alt="" />unavailable</li>
+            <li><img src={mail_icon} alt="" />ghazala.rafi70@gmail.com</li>
             <li><img src={phone_icon} alt="" />+1(202)322-2318</li>
             <li><img src={location_icon} alt="" />6427 Silver Ridge Cir, Alexandria<br/> VA 22315, United States</li>
         </ul>
       </div>
       <div className="contact-col">
-        <form onSubmit={onSubmit}>
-            <label>Your name</label>
-            <input type="text" name='name' placeholder='Enter your name' required/>
-            <label>Phone Number</label>
-            <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
-            <label>Write your messages here (unavailable)</label>
-            <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
-            <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
-        </form>
+        
         <span>{result}</span>
       </div>
     </div>
